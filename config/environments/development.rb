72ceb99b781a8c976590ecc9f67aa2293f33affe
Jesse Oliver Sanford <jos7@columbia.edu>
2010-04-29 15:35:46 -0700

    fixed constant for google

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,4 +16,4 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-GOOGLE_API_KEY = 'ABQIAAAAlAsSFXsiN3MCRMmEvQ2ihBSQ960dr68roU-HxUbRzGITClCuXBQVZCk-GBTlEiD2uQC5fty6M5CRLQ'
+GOOGLE_API_KEY = 'ABQIAAAAlAsSFXsiN3MCRMmEvQ2ihBT7muJXuwH9TG4WmXpOK5eX8zPdpxQG2R7iraxSe9pMr9kYJ4QHMfiOfw'
