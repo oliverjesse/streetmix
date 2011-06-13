6ef125b65d352f8f6f92b94d66ae662cd90eb7c8
Jesse Sanford <jesse@berkeley.edu>
2011-06-13 11:05:50 -0700

    update default route, specify some gem versions

--- Gemfile
+++ Gemfile
@@ -1,13 +1,15 @@
 
-# A sample Gemfile
 source :gemcutter
 #
-gem "rails", "2.3.5"
+gem "rails", "~> 2.3.5"
+gem "sqlite3-ruby", :require => "sqlite3"
 
 gem "paperclip"
 gem "sqlite3-ruby"
+gem "rake", "0.8.7"
 
-gem "rspec"
+gem "rspec", "~> 2.0"
 gem "rspec-rails"
-gem "tzinfo"
-gem "geokit"
+gem "tzinfo", "0.3.24"
+gem 'geokit', '>= 1.5.0'
+gem 'geokit-rails', '1.1.4'
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+# RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -10,7 +10,21 @@ require "rubygems"
 require "bundler"
 Bundler.setup
 
-begin; require 'geokit'; rescue LoadError; end
+# handle incompatibility introduced by rubygems 1.5+
+if Gem::VERSION >= "1.3.6" 
+  module Rails
+    class GemDependency
+      def requirement
+        r = super
+        (r == Gem::Requirement.default) ? nil : r
+      end
+    end
+  end
+end
+
+
+# begin; require 'geokit'; rescue LoadError; end
+
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
@@ -25,7 +39,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "geokit"
+  # config.gem "geokit"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
